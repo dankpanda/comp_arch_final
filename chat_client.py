cafbd11b273649eb061b946111f06f43ca5dfe9c
dankpanda <yowenyo17@gmail.com>
2021-06-20 17:55:47 +0700

    added chatroom commands, /roll, /flip, /me and /help

--- chat_client.py
+++ chat_client.py
@@ -5,21 +5,22 @@ from os import system
 system("")
 address = "localhost"
 port = 10000
+keywords = ["/dc","/roll","/flip","/me","/help"]
 
 while True:
     client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     name = input("Enter username > ")
     client_socket.connect((address,port))
-    print("Connecting to server")
+    print("\nConnecting to server")
     client_socket.send(bytes(name,'utf-8'))
     response = client_socket.recv(256).decode()
-    print("Server response: {}".format(response))
+    print("Server response: {}\n".format(response))
     
     if response == "Connection success":
         break
     else:
         client_socket.close()
-print("Type \"/dc\" to disconnect")
+print("Type \"/help\" for a list of commands")
 
 def receive_message():
     while True:
@@ -33,7 +34,32 @@ t.start()
 while True:
     data = input()
     print ("\033[A                             \033[A")
-    print('You('+name+'): '+data)
+    args = data.split()
+    if(args[0] in keywords):
+        if args[0] == "/help":
+            print("\n/roll    Generates a random number between 0-100")
+            print("/flip    Flip a coin")
+            print("/me      Perform an action in third person")
+            print("/dc      Disconnect from the chatroom")
+            print("/help    Show list of commands\n")
+
+        if args[0] == "/me" and len(args) == 1:
+            print("Invalid syntax for command /me")
+            continue
+        
+        elif data == "/dc":
+            client_socket.send(bytes(data,'utf-8'))
+            break
+
+        elif args[0] == "/dc" and len(args) > 1:
+            print("Invalid syntax for command /dc")
+            continue
+        
+        elif args[0] in ["/roll","/flip"] and len(args) > 1:
+            print("Invalid syntax for command {}".format(args[0]))
+            continue
+    else: 
+        print('You('+name+'): '+data)
+
     client_socket.send(bytes(data,'utf-8'))
-    if(data == "/dc"):
-        break
+    
--- chat_server.py
+++ chat_server.py
@@ -1,10 +1,10 @@
 import socket
 import threading
+import random
 
 address = "localhost"
 port = 10000
 current_clients = []
-
 
 print('Creating server socket')
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -17,14 +17,39 @@ def new_thread(client, address, client_name):
     broadcast_all("-- {} joined the room --".format(client_name))
     while True:
         msg = client.recv(256).decode()
-        if msg == "/dc":
+        if msg.split()[0] == "/help":
+            print(client_name + ": " + msg)
+
+        elif msg == "/dc":
             current_clients.remove([client,client_name])
             print("-- {} has disconnected --".format(client_name))
             broadcast_all("-- {} has disconnected --".format(client_name))
             break
-        message = client_name +': ' + msg
-        print(message)
-        broadcast(message,client)
+
+        elif msg == "/flip":
+            result = random.choice(['HEADS','TAILS'])
+            print("{} flipped \033[1m{}\033[0m".format(client_name,result))
+            broadcast_all("{} flipped \033[1m{}\033[0m".format(client_name,result))
+
+        elif msg == "/roll":
+            result = random.randint(0,100)
+            print("{} rolled \033[1m{}\033[0m".format(client_name,result))
+            broadcast_all("{} rolled \033[1m{}\033[0m".format(client_name,result))
+        
+        elif msg.split()[0] == "/me":
+            result = msg.split()
+            result.pop(0)
+            string = "* " + client_name + " "
+            for i in result:
+                string += i + " "
+            string += "*"
+            print("\033[3m {} \033[0m".format(string))
+            broadcast_all("\033[3m {} \033[0m".format(string))
+
+        else:
+            message = client_name +': ' + msg
+            print(message)
+            broadcast(message,client)
     
 def broadcast(message, sender): 
     for clients in current_clients: 
